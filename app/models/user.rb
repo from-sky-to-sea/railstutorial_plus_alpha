8e090e7fbdc6031ba0113073047ec576df4fe5c6
Kenichi Ukai <ken151102@gmail.com>
2017-08-24 02:37:16 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,25 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  # アカウントを有効にする
+  def activate
+    # update_attribute(:activated,    true)
+    # update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
@@ -41,14 +60,9 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
 
-    # 渡されたトークンがダイジェストと一致したらtrueを返す
-    def authenticated?(remember_token)
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
-
     # ユーザーのログイン情報を破棄する
     def forget
       update_attribute(:remember_digest, nil)
     end
+
 end
